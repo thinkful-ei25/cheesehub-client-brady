130287326ea9b887320b7a09c3b2081da3bb8eae
Box <bradycfox@gmail.com>
2018-11-16 11:27:30 -0500

    re-added some src files

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { connect } from 'react-redux';
+import { fetchUser } from './actions';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(fetchUser());
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1> Hello {this.props.username}</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  console.log(state);
+
+  return {
+    username: state.userReducer.name
+  };
+};
+
+export default connect(mapStateToProps)(App);
